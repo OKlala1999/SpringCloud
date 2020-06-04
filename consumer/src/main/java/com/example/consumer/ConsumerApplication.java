ce9d9c5889d798b72e7eac09afe438f248a53fac
OKlala1999 <17301092@bjtu.edu.cn>
2020-06-04 12:22:14 +0800

    Spring Cloud作业更新 Bus 短路保护 FeignClinet 负载均衡

--- consumer/src/main/java/com/example/consumer/ConsumerApplication.java
+++ consumer/src/main/java/com/example/consumer/ConsumerApplication.java
@@ -1,9 +1,13 @@
 package com.example.consumer;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,6 +16,8 @@ import org.springframework.web.bind.annotation.RestController;
 @EnableEurekaClient
 @RestController
 @EnableFeignClients
+@RefreshScope
+@EnableHystrix
 public class ConsumerApplication {
 
     public static void main(String[] args) {
@@ -21,14 +27,22 @@ public class ConsumerApplication {
     @Autowired
     ProducerDao producerDao;
 
+    @Value("${test}")
+    private String test;
+
     //调用producer微服务
     @RequestMapping(value = "/api/v1/demo/get")
+    @HystrixCommand(fallbackMethod = "errorMsg")
     public String toProducer(){
         return producerDao.toProducer();
     }
 
+    //断路转发
+    public String errorMsg(){
+        return "error!!";
+    }
     @RequestMapping(value = "hello")
     public String Hello(){
-        return "Hello";
+        return test;
     }
 }
